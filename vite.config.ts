0e2daea04b4e992e9d9249946fa746f5827f26d3
slegorreta <64291421+slegorreta@users.noreply.github.com>
2026-05-19 16:36:16 -0600

    Add Vite allowedHosts config

--- vite.config.ts
+++ vite.config.ts
@@ -60,6 +60,8 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    strictPort: true,
+    allowedHosts: true,
     headers: {
       'X-Frame-Options': 'ALLOWALL',
       'Content-Security-Policy': "frame-ancestors *",
